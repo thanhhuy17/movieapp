7ab2d57a721ae5bfd72aa953fcdf4e778a6c77b4
Nguyen Thanh Huy <huymau170819999@gmail.com>
2024-07-02 16:39:20 +0700

    update card 07_02_2024

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,34 @@
+import moment from "moment";
 import { useSelector } from "react-redux";
 
 const Card = ({ data, trending, index }) => {
   const imageURL = useSelector((state) => state.movie.imageURL);
-  //   console.log("Huy Check Index: ", index);
-  console.log("Huy Check True: ", trending && <div>#{index} Trending</div>);
+  //   console.log("Huy Check Release Date: ", data);
+  //   console.log("Huy Check True: ", trending && <div>#{index} Trending</div>);
   return (
-    <div className="w-full max-w-[230px]  h-80 overflow-hidden rounded">
+    <div className="w-full max-w-[230px] h-80 rounded relative overflow-hidden">
       <img src={imageURL + data?.poster_path} alt="" />
 
-      <div className="absolute top-5">
-        {trending && <div>#{index} Trending</div>}
+      <div className="absolute top-4 ">
+        {trending && (
+          // chỗ này bỏ overflow
+          <div className="py-1 px-4 backdrop-blur-3xl rounded-r-full bg-black/20 overflow-hidden">
+            #{index} Trending
+          </div>
+        )}
+      </div>
+
+      <div className="absolute bottom-0 h-14 w-full  bg-black/60 px-2 py-1 backdrop-blur-3xl">
+        <h2 className="text-ellipsis line-clamp-1 text-lg font-semibold">
+          {data?.title || data?.name}
+        </h2>
+
+        <div className="flex justify-between text-neutral-400  items-center">
+          <p>{moment(data?.release_date).format("MMMM Do YYYY")}</p>
+          <p className="bg-black px-2 py-1 rounded-full text-xs">
+            Rating:{Number(data?.vote_average).toFixed(1)}
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,19 +8,25 @@ const Home = () => {
     <div>
       <BannerHome />
       <div className="container mx-auto px-3 my-10">
-        <h2 className="text-xl lg:text-2xl font-bold mb-3">Trending Show</h2>
-        <div className="grid grid-cols-[repeat(auto-fit,230px)] gap-6">
-          {trendingData.map((data, index) => {
-            return (
-              // eslint-disable-next-line react/jsx-key
-              <Card
-                key={data.id}
-                data={data}
-                index={index + 1}
-                trending={true}
-              />
-            );
-          })}
+        <h2 className="text-xl lg:text-2xl font-bold mb-3 text-white">
+          Trending Show
+        </h2>
+
+        <div>
+          {/* <div className="grid grid-cols-[repeat(auto-fit,230px)]  grid-flow-col gap-6 overflow-x-scroll"> */}
+          <div className="grid grid-cols-[repeat(auto-fit,230px)] gap-6 grid-flow-col">
+            {trendingData.map((data, index) => {
+              return (
+                // eslint-disable-next-line react/jsx-key
+                <Card
+                  key={data.id}
+                  data={data}
+                  index={index + 1}
+                  trending={true}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
